ee6daaf9db8ec9dd324067ff05110a79ec15c021
Evgeny Muravev <lovesexdance1992@gmail.com>
2023-06-19 23:18:43 +0300

    merged

--- Lesson7/Seminar.py
+++ Lesson7/Seminar.py
@@ -17,25 +17,93 @@ import random
 # transormed_values = list(map(transformation, values))
 # print(values)
 # print(transormed_values)
+# _________________________________________________________________________________________
+
+
+# Задача №1
+# У вас есть код, который вы не можете менять (так часто бывает, когда код в глубине
+# программы используется множество раз и вы не хотите ничего сломать):
+# transformation = <???>
+# values = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] # или любой другой список
+# transormed_values = list(map(transformation, values))
+# Единственный способ вашего взаимодействия с этим кодом - посредством задания
+# функции transformation.
+# Однако вы поняли, что для вашей текущей задачи вам не нужно никак преобразовывать
+# список значений, а нужно получить его как есть.
+# Напишите такое лямбда-выражение transformation, чтобы transformed_values получился
+# копией values.
 
 
 # mapped_numbers = list(map(lambda x: x * 2 + 3, values))
 # print(mapped_numbers)
 # функция map - позволяет менять каждое значение в списках не создавая его копию, что улучшает производительность кода. 
 
-size = int(input("Введите размер списка: "))
-myList = [0]*size
+# size = int(input("Введите размер списка: "))
+# myList = [0]*size
 
-for i in range(size):
-    myList[i] = random.randint(20, 25)
-print(myList)
-transformation = lambda x : x - 15
-newList = list(map(transformation,myList))
-print(newList)
+# for i in range(size):
+#     myList[i] = random.randint(20, 25)
+# print(myList)
+# transformation = lambda x : x - 15
+# newList = list(map(transformation,myList))
+# print(newList)
 
-for i in range(len(newList)) :
-    if newList[i] == max(newList):
-        newList[i] = min(newList)
-        print(newList[i])
+# for i in range(len(newList)) :
+#     if newList[i] == max(newList):
+#         newList[i] = min(newList)
 
-print(sorted(newList))
+# print(newList)
+# _________________________________________________________________________________________
+
+
+# Задача №2
+# Планеты вращаются вокруг звезд по эллиптическим орбитам.
+# Назовем самой далекой планетой ту, орбита которой имеет
+# самую большую площадь. Напишите функцию
+# find_farthest_orbit(list_of_orbits), которая среди списка орбит
+# планет найдет ту, по которой вращается самая далекая
+# планета. Круговые орбиты не учитывайте: вы знаете, что у
+# вашей звезды таких планет нет, зато искусственные спутники
+# были были запущены на круговые орбиты. Результатом
+# функции должен быть кортеж, содержащий длины полуосей
+# эллипса орбиты самой далекой планеты. Каждая орбита
+# представляет из себя кортеж из пары чисел - полуосей ее
+# эллипса. Площадь эллипса вычисляется по формуле S = pi*a*b,
+# где a и b - длины полуосей эллипса. При решении задачи
+# используйте списочные выражения. Подсказка: проще всего
+# будет найти эллипс в два шага: сначала вычислить самую
+# большую площадь эллипса, а затем найти и сам эллипс,
+# имеющий такую площадь. Гарантируется, что самая далекая
+# планета ровно одна
+
+# orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
+
+# elips = list(filter(lambda item: item[0] != item[1], orbits))
+# sorbits = tuple(map(lambda x: x[0] * x[1],elips))
+# print(sorbits)
+# print(elips[sorbits.index(max(sorbits))])
+
+
+# Задача №3
+# Напишите функцию same_by(characteristic, objects), которая
+# проверяет, все ли объекты имеют одинаковое значение
+# некоторой характеристики, и возвращают True, если это так.
+# Если значение характеристики для разных объектов
+# отличается - то False. Для пустого набора объектов, функция
+# должна возвращать True. Аргумент characteristic - это
+# функция, которая принимает объект и вычисляет его
+# характеристику.
+
+# def same_by(characteristic,objects):
+#     for item in objects:
+#         if not characteristic(item):
+#             return False
+#     return True
+
+
+# if same_by(lambda x: x %2 == 0,values):
+#     print('same')
+# else:
+#     print('different')
+
+
